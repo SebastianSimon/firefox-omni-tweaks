dcf4529ec8583800579b28f5fab165d13a8cff1c
Sebastian Simon <37915283+SebastianSimon@users.noreply.github.com>
2020-07-31 08:45:08 +0200

    Suppress expected unzip errors (fix #5); increase code quality

--- Firefox Selection Fix.sh
+++ Firefox Selection Fix.sh
@@ -3,50 +3,67 @@
 # Script repo: https://github.com/SebastianSimon/firefox-selection-fix
 # See https://superuser.com/a/1559926/751213 for detailed explanation.
 
-return_wd="$PWD"
-reason_already_root='already_root'
-description='The Firefox Selection Fix script disables the broken clickSelectsAll behavior of Firefox. Make sure Firefox is up-to-date and closed'
+readonly return_wd="${PWD}"
+readonly reason_already_root='already_root'
+readonly description='The Firefox Selection Fix script disables the broken clickSelectsAll behavior of Firefox. Make sure Firefox is up-to-date and closed'
 firefox_dir=$(whereis firefox | cut -d ' ' -f 2)
 fallback_firefox_dir='/usr/lib/firefox' # Fallback path: put your Firefox install path here. The install path includes the `firefox` binary and a `browser` directory.
 
-function require_root(){
-  if [[ $(id -u) -ne 0 ]]; then
-    sudo FIXFX_SUPPRESS_DESCRIPTION=true "${BASH_SOURCE[0]}"
-    
-    exit $?
+function check_firefox_path(){
+  if [[ ! -f "${firefox_dir}/browser/omni.ja" ]]; then
+    firefox_dir="${fallback_firefox_dir}"
   fi
   
-  fix_firefox
+  if [[ ! -f "${firefox_dir}/browser/omni.ja" ]]; then
+    echo "Error: Firefox install path not found in '${firefox_dir}'." >&2
+    
+    return 1
+  fi
+  
+  return 0
 }
 
 function check_root_required(){
   if [[ $(id -u) -eq 0 ]]; then
-    echo "$reason_already_root"
+    echo "${reason_already_root}"
     
     return
   fi
   
-  for path in "$firefox_dir/browser" "$firefox_dir/browser/omni.ja" '/tmp'; do
-    if [[ ! -w "$path" ]]; then
-      echo "$path"
+  for path in "${firefox_dir}/browser" "${firefox_dir}/browser/omni.ja" '/tmp'; do
+    if [[ ! -w "${path}" ]]; then
+      echo "${path}"
       
       return
     fi
   done
 }
 
-function check_firefox_path(){
-  if [[ ! -f "$firefox_dir/browser/omni.ja" ]]; then
-    firefox_dir="$fallback_firefox_dir"
+function require_root(){
+  if [[ $(id -u) -ne 0 ]]; then
+    sudo FIXFX_SUPPRESS_DESCRIPTION=true "${BASH_SOURCE[0]}"
+    
+    exit
   fi
   
-  if [[ ! -f "$firefox_dir/browser/omni.ja" ]]; then
-    echo "Error: Firefox install path not found in '$firefox_dir'." >&2
+  fix_firefox
+}
+
+function unzip_without_expected_errors(){
+  local -r unzip_errors="$(unzip -q "${firefox_dir}/browser/omni.ja" -d omni 2>&1)"
+  local -r expected_errors='^warning.+?\[.*?omni\.ja\]:.+?[1-9][0-9]*.+?extra.+?bytes.+?attempting.+?anyway.+?error.+?\[.*?omni\.ja\]:.+?reported.+?length.+?-[1-9][0-9]*.+?bytes.+?long.+?Compensating\.{3}$'
+  
+  if (echo "${unzip_errors}" | xargs | grep --extended-regexp --quiet "${expected_errors}"); then
+    return 0
+  else
+    echo
+    echo -e "\033[0;91mUnexpected error in unzip; terminating.\033[0m" >&2
+    echo "${unzip_errors}" >&2
+    rm -r omni
+    cd "${return_wd}" || exit
     
     return 1
   fi
-  
-  return 0
 }
 
 function fix_firefox(){
@@ -57,49 +74,47 @@ function fix_firefox(){
     read -p 'Create backup of omni.ja before applying the fix? This overwrites the old backup! [y/N] ' -r create_backup
   fi
   
-  if [[ "$create_backup" =~ ^[Yy]$ ]]; then
-    echo "Copying '$firefox_dir/browser/omni.ja' to '/tmp/omni.ja~'."
-    cp -p "$firefox_dir/browser/omni.ja" /tmp/omni.ja~
+  if [[ "${create_backup}" =~ ^[Yy]$ ]]; then
+    echo "Copying '${firefox_dir}/browser/omni.ja' to '/tmp/omni.ja~'."
+    cp -p "${firefox_dir}/browser/omni.ja" /tmp/omni.ja~
   fi
   
-  echo "Fixing Firefox: '$firefox_dir'."
-  echo
+  echo "Fixing Firefox: '${firefox_dir}'."
   cd /tmp || exit
   mkdir omni
-  unzip -q "$firefox_dir/browser/omni.ja" -d omni
+  unzip_without_expected_errors || exit
   sed -i 's/this\._preventClickSelectsAll = this\.focused;/this._preventClickSelectsAll = true;/' omni/modules/UrlbarInput.jsm
   sed -i 's/this\._preventClickSelectsAll = this\._textbox\.focused;/this._preventClickSelectsAll = true;/' omni/chrome/browser/content/browser/search/searchbar.js
   sed -i 's/return shortkey\.split[(]"_"[)]\[1\];/return (shortkey || "_").split("_")[1];/' omni/chrome/devtools/modules/devtools/client/definitions.js # See https://github.com/SebastianSimon/firefox-selection-fix/issues/2
   cd omni || exit
   zip -qr9XD omni.ja ./*
   cd ..
-  mv omni/omni.ja "$firefox_dir/browser/omni.ja" || exit
-  chown --reference=omni.ja~ "$firefox_dir/browser/omni.ja"
-  chmod --reference=omni.ja~ "$firefox_dir/browser/omni.ja"
+  mv omni/omni.ja "${firefox_dir}/browser/omni.ja" || exit
+  chown --reference=omni.ja~ "${firefox_dir}/browser/omni.ja"
+  chmod --reference=omni.ja~ "${firefox_dir}/browser/omni.ja"
   rm -r omni
-  touch "$firefox_dir/browser/.purgecaches"
-  cd "$return_wd" || exit
-  echo
+  touch "${firefox_dir}/browser/.purgecaches"
+  cd "${return_wd}" || exit
   echo 'Your Firefox should now be able to run with an improved user experience! Start Firefox and try it out.'
   read -p 'Press [Enter] to exit. If Firefox does not run properly, restore the backup by pressing [r], then [Enter]. ' -r restore_backup
   
-  if [[ "$restore_backup" =~ [Rr] ]]; then
-    echo "Copying '/tmp/omni.ja~' to '$firefox_dir/browser/omni.ja'."
-    cp -p /tmp/omni.ja~ "$firefox_dir/browser/omni.ja"
-    touch "$firefox_dir/browser/.purgecaches"
+  if [[ "${restore_backup}" =~ [Rr] ]]; then
+    echo "Copying '/tmp/omni.ja~' to '${firefox_dir}/browser/omni.ja'."
+    cp -p /tmp/omni.ja~ "${firefox_dir}/browser/omni.ja"
+    touch "${firefox_dir}/browser/.purgecaches"
   else
     echo 'You can restore the backup later on by typing these two commands:'
-    echo -e "\033[0;40;96mcp -p /tmp/omni.ja~ $firefox_dir/browser/omni.ja"
-    echo -e "touch $firefox_dir/browser/.purgecaches\033[0m"
+    echo -e "\033[0;40;96mcp -p /tmp/omni.ja~ ${firefox_dir}/browser/omni.ja"
+    echo -e "touch ${firefox_dir}/browser/.purgecaches\033[0m"
     echo "You can also copy the file '/tmp/omni.ja~' to another backup location."
   fi
   
   bash
 }
 
-check_firefox_path || exit $?
+check_firefox_path || exit
 
-if [[ -f "$firefox_dir/browser/.purgecaches" ]]; then
+if [[ -f "${firefox_dir}/browser/.purgecaches" ]]; then
   echo "Error: You need to start and close Firefox again to apply the changes before running this script." >&2
   
   exit 1
@@ -107,17 +122,18 @@ fi
 
 root_required_reason="$(check_root_required)"
 
-if [[ ! "$FIXFX_SUPPRESS_DESCRIPTION" ]]; then
-  if [[ "$root_required_reason" && $(id -u) -ne 0 ]]; then
-    echo "$description."
+if [[ ! "${FIXFX_SUPPRESS_DESCRIPTION}" ]]; then
+  if [[ "${root_required_reason}" && $(id -u) -ne 0 ]]; then
+    echo "${description}."
   else
-    read -p "$description, then press [Enter] to continue. "
+    # shellcheck disable=SC2162
+    read -p "${description}, then press [Enter] to continue. "
   fi
 fi
 
-if [[ "$root_required_reason" ]]; then
-  if [[ "$root_required_reason" != "$reason_already_root" ]]; then
-    echo "Continue as root: write access to '$root_required_reason' is required."
+if [[ "${root_required_reason}" ]]; then
+  if [[ "${root_required_reason}" != "${reason_already_root}" ]]; then
+    echo "Continue as root: write access to '${root_required_reason}' is required."
   fi
   
   require_root
