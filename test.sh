655f8067584d0b7b59667a9b89ed5e3a599ffbb9
Sebastian Simon <sebastian.simon1602@gmail.com>
2026-03-13 11:13:08 +0100

    Add ShellCheck to tests

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC1111
 
 readonly test_firefox_source="${1}"
 test_status='0'
@@ -19,6 +20,44 @@ print_failed(){
 
 print_passed(){
   echo "$(tput setaf 2)⏺ PASSED$(tput sgr0): ${1}"
+}
+
+test_test_script_passes_shellcheck(){
+  local -r test_name='Test script passes ShellCheck'
+  local -r test_dir='./test/test_shellcheck'
+
+  mkdir --parents -- "${test_dir}"
+  cp --recursive -- "${test_firefox_source}"/* "${test_dir}"
+
+  shellcheck --color=always ./test.sh >"${test_dir}.stdout"
+
+  if [[ -n "$(cat "${test_dir}.stdout")" ]]; then
+    print_failed "“${test_name}”; output:"
+    cat "${test_dir}.stdout"
+
+    return '1'
+  fi
+
+  print_passed "${test_name}"
+}
+
+test_fixfx_script_passes_shellcheck(){
+  local -r test_name='FixFx script passes ShellCheck'
+  local -r test_dir='./test/fixfx_shellcheck'
+
+  mkdir --parents -- "${test_dir}"
+  cp --recursive -- "${test_firefox_source}"/* "${test_dir}"
+
+  shellcheck --color=always ./fixfx.sh >"${test_dir}.stdout"
+
+  if [[ -n "$(cat "${test_dir}.stdout")" ]]; then
+    print_failed "“${test_name}”; output:"
+    cat "${test_dir}.stdout"
+
+    return '1'
+  fi
+
+  print_passed "${test_name}"
 }
 
 test_all_options_produce_no_warnings(){
@@ -59,6 +98,8 @@ test_all_options_except_tab_switch_copies_to_clipboard_produce_no_warnings(){
   print_passed "${test_name}"
 }
 
+test_test_script_passes_shellcheck || set_failed
+test_fixfx_script_passes_shellcheck || set_failed
 test_all_options_produce_no_warnings || set_failed
 test_all_options_except_tab_switch_copies_to_clipboard_produce_no_warnings || set_failed
 rm --recursive -- './test'
